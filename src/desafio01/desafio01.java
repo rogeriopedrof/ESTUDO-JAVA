5a013dd87b485c3954bef285b02d3e053f5435cc
rogeriopedrof <rrodev25@gmail.com>
2025-03-05 14:26:34 -0300

    ajustes em "desafio1.java"

--- src/desafio01/desafio01.java
+++ src/desafio01/desafio01.java
@@ -3,6 +3,15 @@ package desafio01;
 public class desafio01 {
     public static void main(String[] args) {
 
+
+        /* Lógica do ifs:
+
+         * Com um poder de luta inferior a X, o guerreiro consegue vencer o vilão, desde que o vilão não esteja em um
+         * nível superior ao esperado. Caso isso aconteça, o guerreiro é derrotado facilmente. Agora, se o guerreiro
+         * tiver um poder superior a X, ele vence facilmente.
+         */
+
+
         // GUERREIRO1
         String nome = "Piccolo";
         float PL = 1.3f;
@@ -18,6 +27,20 @@ public class desafio01 {
         System.out.println("Status da missão: " + status);
         System.out.println("Nível de dificuldade: " + ND);
 
+        if (PL < 2.3) {
+            if (ND == 'B' || ND == 'A' || ND == 'S') { //OPs lógicos: && (and), ||(ou) e !(not)
+                status = "1.Piccolo foi DERROTADO com facilidade!";
+                System.out.println(status);
+            } else {
+                status = "1.Raditz foi DERROTADO";
+                System.out.println(status);
+            }
+        } else {
+            status = "1.Raditz foi DERROTADO com facilidade!";
+            System.out.println(status);
+        }
+
+
         // GUERREIRO2
         String nome2 = "Tenshihan";
         float PL2 = 1.8f;
@@ -32,6 +55,20 @@ public class desafio01 {
         System.out.println("Missão: " + missao2);
         System.out.println("Status da missão: " + status2);
         System.out.println("Nível de dificuldade: " + ND2);
+
+        if (PL2 < 8.0) {
+            if (ND2 == 'B' || ND2 == 'A' || ND == 'S') {
+                status2 = "2.Tenshinhan foi DERROTADO com facilidade!";
+                System.out.println(status2);
+            } else {
+                status2 = "2.Nappa foi DERROTADO";
+                System.out.println(status2);
+            }
+        } else {
+            status2 = "2.Nappa foi DERROTADO com facilidade!";
+            System.out.println(status2);
+        }
+
 
         // GUERREIRO3
         String nome3 = "Goku";
@@ -54,38 +91,6 @@ public class desafio01 {
         System.out.println("Resultado das Lutas:\n"); // '\n' quebra a linha!
 
 
-        /* Explicando a lógica:
-
-         * Com um poder de luta inferior a X, o guerreiro consegue vencer o vilão, desde que o vilão não esteja em um
-         * nível superior ao esperado. Caso isso aconteça, o guerreiro é derrotado facilmente. Agora, se o guerreiro
-         * tiver um poder superior a X, ele vence facilmente.
-         */
-
-        if (PL < 2.3) {
-            if (ND == 'B' || ND == 'A' || ND == 'S') { //OPs lógicos: && (and), ||(ou) e !(not)
-                status = "1.Piccolo foi DERROTADO com facilidade!";
-                System.out.println(status);
-            } else {
-                status = "1.Raditz foi DERROTADO";
-                System.out.println(status);
-            }
-        } else {
-            status = "1.Raditz foi DERROTADO com facilidade!";
-            System.out.println(status);
-        }
-
-        if (PL2 < 8.0) {
-            if (ND2 == 'B' || ND2 == 'A' || ND == 'S') {
-                status2 = "2.Tenshinhan foi DERROTADO com facilidade!";
-                System.out.println(status2);
-            } else {
-                status2 = "2.Nappa foi DERROTADO";
-                System.out.println(status2);
-            }
-        } else {
-            status2 = "2.Nappa foi DERROTADO com facilidade!";
-            System.out.println(status2);
-        }
         if (PL3 < 17.0) {
             if (ND3 == 'S') {
                 status3 = "3.Goku foi DERROTADO com facilidade!";
